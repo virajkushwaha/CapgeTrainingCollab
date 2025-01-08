4e820bbd812f417e193874bfe7e94bd3fa15350d
virajkushwaha <virajkushwaha21@gmail.com>
2025-01-08 14:59:33 +0530

    changing

--- calculator.java
+++ calculator.java
@@ -1,12 +1,15 @@
 import java.util.Scanner;
 class Calculator{
 	public static void main (String args[]){
+		int sub(int a , int b ){
+			return a-b;
+		}
 		System.out.println("Enter the numbers");
 		Scanner sc = new Scanner(System.in);
 		int x = sc.nextInt();
 		int y = sc.nextInt();
 		System.out.println("Enter the Operator");
 		String op = sc.next();
-		System.out.println(x+" "+op+" "+y);
+		
 	}
 }
